5cd0192b71eb7ab94c2f9cc43dd82e9a9c6beef0
112batuhan <batuhancakir97@hotmail.com>
2024-11-16 23:03:24 +0300

    feat: activity documentation wrappers

--- src/schema_with.rs
+++ src/schema_with.rs
@@ -6,19 +6,35 @@ use crate::{database::user::UserSmall, osu_api::OsuBeatmapSmall};
 #[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
 pub struct FlattenedActivityType {
     pub event_type: EventType,
-    /// Influenced user. `UserSmall` type. For `ADD_INFLUENCE`, `REMOVE_INFLUENCE`,
-    /// `ADD_INFLUENCE_BEATMAP`, `REMOVE_INFLUENCE_BEATMAP`, `EDIT_INFLUENCE_DESC`,
-    /// `EDIT_INFLUENCE_TYPE` activity types.
-    pub influence: Option<UserSmall>,
-    /// Added or removed beatmap. `OsuBeatmapSmall` type. For `ADD_USER_BEATMAP`, `REMOVE_USER_BEATMAP`,
-    /// `ADD_INFLUENCE_BEATMAP`, `REMOVE_INFLUENCE_BEATMAP` activity types.
-    pub beatmap: Option<OsuBeatmapSmall>,
+    pub influence: Option<UserSmallActivity>,
+    pub beatmap: Option<OsuBeatmapSmallActivity>,
     /// Changed influence description. for `EDIT_INFLUENCE_DESC` activity type.
     pub description: Option<String>,
     /// Changed influence type. for `EDIT_INFLUENCE_TYPE` activity type.
     pub influence_type: Option<u8>,
     /// Changed bio. For `EDIT_BIO` activity type.
     pub bio: Option<String>,
+}
+
+/// Influenced user. `UserSmall` type. For `ADD_INFLUENCE`, `REMOVE_INFLUENCE`,
+/// `ADD_INFLUENCE_BEATMAP`, `REMOVE_INFLUENCE_BEATMAP`, `EDIT_INFLUENCE_DESC`,
+/// `EDIT_INFLUENCE_TYPE` activity types.
+///
+/// This is a placeholder type for documentation only. It's the same as `UserSmall`
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct UserSmallActivity {
+    #[serde(flatten)]
+    inner: UserSmall,
+}
+
+/// Added or removed beatmap. `OsuBeatmapSmall` type. For `ADD_USER_BEATMAP`, `REMOVE_USER_BEATMAP`,
+/// `ADD_INFLUENCE_BEATMAP`, `REMOVE_INFLUENCE_BEATMAP` activity types.
+///
+/// This is a placeholder type for documentation only. It's the same as `OsuBeatmapSmall`
+#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
+pub struct OsuBeatmapSmallActivity {
+    #[serde(flatten)]
+    inner: OsuBeatmapSmall,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
